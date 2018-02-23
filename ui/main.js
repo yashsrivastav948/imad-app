c34c9ddb246976e72c6cd63b2aae986487f79738
yashsrivastav948 <30635013+yashsrivastav948@users.noreply.github.com>
2018-02-23 13:43:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+//changing the text in html
 var element =document.getElementById('text');
 element.innerHTML='new text inserted by javascipt';
+//moving the image 
+var img=document.getElementById('image');
+img.onclick=function(){
+  img.style.marginLeft='100px';  
+};
