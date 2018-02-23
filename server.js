7d88edb140903ab9697c87b98cadcdc2880b9dca
yashsrivastav948 <30635013+yashsrivastav948@users.noreply.github.com>
2018-02-23 13:34:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,35 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var aa={ 
+title:'articl_eone',
+    content:`
+       <p>this is paragraph one</p>
+       <p>this page is created my</p>
+       <p>my name is yash srivastava and i am from raebareli, i am doing btech from bharati vidyapeeth pune..and 
+       i learning how to develop app by a good online course by imad hasura</p>`
+       
+};
 
+function createtemp(data){
+    var title=data.title;
+    
+var content=data.content;
+var html=`<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+        <title>${title}</title> 
+        <meta name="viewport" content="width=device-width, initial-scale=1"/> 
+    
+    </head>
+    
+    <body>
+    ${content}
+    </body>
+</html>
+`;
+    return createtemp;
+}
 app.get('/a.html', function (req, res) {
 res.send('hello world');
 });
