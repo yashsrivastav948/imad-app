257ed35749c228160f7c88bde42f895a9536c8a9
yashsrivastav948 <30635013+yashsrivastav948@users.noreply.github.com>
2018-02-23 19:42:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,11 +6,11 @@ element.innerHTML='new text inserted by javascipt';
 var img=document.getElementById('image');
 var marginLeft=0;
 function moveright(){
-    marginLeft=marginLeft + 10;
+    marginLeft=marginLeft + 5;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
   //img.style.marginLeft='100px';  
-var interval=setInterval(moveright, 100);
+var interval=setInterval(moveright, 50);
     
 };
